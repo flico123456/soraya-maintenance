e3adb44f0a6cb61d1e50eb9a070ae2a0ad8a48c2
Ruben JACQZ <rubenjacqz16@gmail.com>
2023-11-30 14:12:07 +0100

    2  commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+    async redirects() {
+      return [
+        {
+          source: '/fr/maillot-de-bain-femme/maillots-de-bain-1-piece.html',
+          destination: '/',
+          permanent: true,
+        },
+      ]
+    },
+    nextConfig
+  }
